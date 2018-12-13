89b872bf36ad916f9f2c3892bc1967b3c553419f
Nicolas Frankel <nicolas.frankel@exoscale.com>
2018-12-13 10:12:28 +0100

    Replace Kotlin sample with Java sample

--- exoscale-api/src/test/kotlin/TestApi.kt
+++
@@ -1,8 +0,0 @@
-import com.exoscale.api.*
-
-fun main(args: Array<String>) {
-    val command = ListAccounts()
-    val execute = withDefaultAccount()
-    val result = execute(command)
-    println(result.listaccountsresponse.Account[0].cpuavailable)
-}
